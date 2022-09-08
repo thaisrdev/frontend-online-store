25cbe081875f407c78d7a4589f03eb124af29cb8
Caroline Nunes <nunescaroline905@gmail.com>
2022-09-08 18:04:57 -0300

    requisito 2 e 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Main from './components/Main';
+import Cart from './components/Cart';
 
 export default class App extends Component {
   render() {
@@ -8,6 +9,7 @@ export default class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Main } />
+          <Route path="/Cart" component={ Cart } />
         </Switch>
       </BrowserRouter>
     );
