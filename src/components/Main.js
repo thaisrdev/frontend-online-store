130218478849acf159c4a352e7883b7aa3ed3127
Thais Rosa <tsrosa18@gmail.com>
2022-09-09 15:18:49 -0300

    requisito 4

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 export default class Main extends Component {
+  state = {
+    categories: [],
+  };
+
+  componentDidMount() {
+    getCategories().then((response) => this.setState({ categories: response }));
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
+        { categories.map((element) => (
+          <p data-testid="category" key={ element.id }>
+            {' '}
+            {element.name}
+            {' '}
+          </p>))}
 
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
