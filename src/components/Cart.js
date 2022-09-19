180a492718b72750e69864c2c23786084665cd4d
Thais Rosa <tsrosa18@gmail.com>
2022-09-18 21:07:04 -0300

    increase and decrease buttons

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -19,23 +19,55 @@ class Cart extends Component {
   removeProduct = (parametro) => {
     const { cartProducts } = this.state;
     const findByIndex = cartProducts.findIndex((element) => element.id === parametro);
-    const updatedArray = [...cartProducts.slice(0, findByIndex),
+    const updatedArray = [...cartProducts.slice(0, findByIndex), // no slice o segundo valor é não incluso
       ...cartProducts.slice((findByIndex + 1), (cartProducts.length))];
     this.setState({ cartProducts: updatedArray });
   };
 
+  increaseQuantity = (parametro) => {
+    const { cartProducts } = this.state;
+    const updatedArray = cartProducts.map((element) => {
+      const { available_quantity: availableQuantity } = element;
+      if (element.id === parametro && availableQuantity > element.order_backend) {
+        element.order_backend += 1;
+        return element;
+      }
+      return element;
+    });
+    this.setState({
+      cartProducts: updatedArray,
+    });
+  };
+
+  decreaseQuantity = (parametro) => {
+    const { cartProducts } = this.state;
+    const produto = cartProducts.find((element) => element.id === parametro);
+    if (produto.order_backend > 1) {
+      produto.order_backend -= 1;
+    }
+    if (produto.order_backend === 1) {
+      this.removeProduct(produto.id);
+    }
+    this.setState({
+      cartProducts,
+    });
+  };
+
   render() {
     const { cartProducts } = this.state;
-    const quantity = 1; // mudar pra let se conseguir fazer adição e subtração
+    // const quantity = 1; // mudar pra let se conseguir fazer adição e subtração
     return (
       <div>
         {
           cartProducts.map((element) => (
             <div key={ element.id }>
               <p data-testid="shopping-cart-product-name">{ element.title }</p>
-              <p data-testid="shopping-cart-product-quantity">{ quantity }</p>
+              <p data-testid="shopping-cart-product-quantity">
+                { element.order_backend }
+
+              </p>
               <p>{ element.price }</p>
-              {/* <button
+              <button
                 type="button"
                 data-testid="product-increase-quantity"
                 onClick={ () => this.increaseQuantity(element.id) }
@@ -50,7 +82,7 @@ class Cart extends Component {
               >
                 -
 
-              </button> */}
+              </button>
               <button
                 type="button"
                 data-testid="remove-product"
